7498f65bc3bb5efc8cb8f55397c1943bef44b0d4
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-08 15:54:09 +0200

    added npm packages

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/about', (req, res) => res.send('This is the about page'))
+app.get('/contact', (req, res) => res.send('This is the contact page'))
+app.listen(port, () => console.log('Example app listening at http://localhost:${port}'))
+app.get('*', (req, res) => res.send('ERROR 404'))
