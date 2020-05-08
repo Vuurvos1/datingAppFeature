ade3656e0c140cd25b89f3ae94348cf232090601
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-08 23:42:39 +0200

    setting up express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.get('/home', (req, res) => res.sendFile(__dirname + '/index.html'))
+app.use(express.static(__dirname + ''));
+
 app.get('/about', (req, res) => res.send('This is the about page'))
 app.get('/contact', (req, res) => res.send('This is the contact page'))
-app.listen(port, () => console.log('Example app listening at http://localhost:${port}'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 app.get('*', (req, res) => res.send('ERROR 404'))
