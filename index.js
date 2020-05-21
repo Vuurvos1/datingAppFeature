0f22fca22c7db4289f9dde0d82a9cfdfb598a628
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-21 16:31:32 +0200

    sending messages to the server

--- index.js
+++ index.js
@@ -1,55 +1,59 @@
 const express = require('express')
+const slug = require('slug');
+const bodyParser = require('body-Parser');
 const app = express()
 const port = 3000
 
-let data = [
-  {
-    id: '0001',
-    name: 'Hanze',
-    imgFullRes: 'img/profilePicture1x512.jpg',
-    imgHalfRes: 'img/profilePicture1x256.jpg',
+let data = [{
+        id: '0001',
+        name: 'Hanze',
+        imgFullRes: 'img/profilePicture1x512.jpg',
+        imgHalfRes: 'img/profilePicture1x256.jpg',
 
-    messages: [
-        {send: 'false', message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.`},
-        {send: 'true', message: `Hey, we matched!`},
-        {send: 'false', message: `Hey, How are you?`}
-    ]
-  },
-  {
-    id: '0002',
-    name: 'Marielle',
-    imgFullRes: 'img/profilePicture2x512.jpg',
-    imgHalfRes: 'img/profilePicture2x256.jpg',
+        messages: [
+            { send: 'false', message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.` },
+            { send: 'true', message: `Hey, we matched!` },
+            { send: 'false', message: `Hey, How are you?` }
+        ]
+    },
+    {
+        id: '0002',
+        name: 'Marielle',
+        imgFullRes: 'img/profilePicture2x512.jpg',
+        imgHalfRes: 'img/profilePicture2x256.jpg',
 
-    messages: [
-        {send: 'false', message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.`},
-        {send: 'true', message: `Hey, we matched!`},
-        {send: 'false', message: `Hey, How are you?`}
-    ]
-  },
-  {
-    id: '0003',
-    name: 'Hanze',
-    imgFullRes: 'img/profilePicture1x512.jpg',
-    imgHalfRes: 'img/profilePicture1x256.jpg',
+        messages: [
+            { send: 'false', message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.` },
+            { send: 'true', message: `Hey, we matched!` },
+            { send: 'false', message: `Hey, How are you?` }
+        ]
+    },
+    {
+        id: '0003',
+        name: 'Hanze',
+        imgFullRes: 'img/profilePicture1x512.jpg',
+        imgHalfRes: 'img/profilePicture1x256.jpg',
 
-    messages: [
-        {send: 'false', message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.`},
-        {send: 'true', message: `Hey, we matched!`},
-        {send: 'false', message: `Hey, How are you?`}
-    ]
-  }
+        messages: [
+            { send: 'false', message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.` },
+            { send: 'true', message: `Hey, we matched!` },
+            { send: 'false', message: `Hey, How are you?` }
+        ]
+    }
 ];
 
 app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/', sendMsg);
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 
 app.get('/index', (req, res) => {
-    res.render('index.ejs', {data: data});
+    res.render('index.ejs', { data: data });
     console.log(`${req.headers.host}/${req.url}`);
 })
 
@@ -63,14 +67,14 @@ app.get('*', (req, res) => {
 })
 
 
+function sendMsg(req, res) {
+    let msg = {
+        send: 'true',
+        message: req.body.message
+    }
+
+    data[0].messages.push(msg)
 
 
-/* Old */
-// app.get('/home', (req, res) => res.sendFile(__dirname + '/index.html'))
-// app.use(express.static(__dirname + ''));
-
-// app.get('/about', (req, res) => res.send('This is the about page'))
-// app.get('/contact', (req, res) => res.send('This is the contact page'))
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-// app.get('*', (req, res) => res.send('ERROR 404'))
-
+    res.redirect('/index');
+}
