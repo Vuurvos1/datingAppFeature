083c30e1e3235b183cd32c8fe40ff7c3c5206be4
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-27 16:25:42 +0200

    writing to the mongodb database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongo = require('mongodb');
+const ObjectId = mongo.ObjectID;
+
 // const slug = require('slug')
 const bodyParser = require('body-Parser');
 require('dotenv').config();
@@ -7,63 +9,30 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
+// Data structure
 // const data = [
 //   {
 //     id: '0001',
-//     name: 'Hanze',
+//     name: 'Name',
 //     imgFullRes: 'img/profilePicture1x512.jpg',
 //     imgHalfRes: 'img/profilePicture1x256.jpg',
 
 //     messages: [
-//       {
-//         send: 'false',
-//         message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.`,
-//       },
-//       { send: 'true', message: `Hey, we matched!` },
-//       { send: 'false', message: `Hey, How are you?` },
-//     ],
-//   },
-//   {
-//     id: '0002',
-//     name: 'Marielle',
-//     imgFullRes: 'img/profilePicture2x512.jpg',
-//     imgHalfRes: 'img/profilePicture2x256.jpg',
-
-//     messages: [
-//       {
-//         send: 'false',
-//         message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.`,
-//       },
-//       { send: 'true', message: `Hey, we matched!` },
-//       { send: 'false', message: `Hey, How are you?` },
-//     ],
-//   },
-//   {
-//     id: '0003',
-//     name: 'Hanze',
-//     imgFullRes: 'img/profilePicture1x512.jpg',
-//     imgHalfRes: 'img/profilePicture1x256.jpg',
-
-//     messages: [
-//       {
-//         send: 'false',
-//         message: `According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those.`,
-//       },
 //       { send: 'true', message: `Hey, we matched!` },
 //       { send: 'false', message: `Hey, How are you?` },
 //     ],
 //   },
 // ];
 
-let db = null;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}-8rusk.azure.mongodb.net/test?retryWrites=true&w=majority`;
+let db;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
 
 mongo.MongoClient.connect(uri, function (err, client) {
   db = client.db('DatingApp');
   db.collection('Users', function (err, collection) {
     collection.find().toArray(function (err, items) {
       if (err) throw err;
-      console.log(items);
+      // console.log(items);
     });
   });
 });
@@ -79,8 +48,13 @@ app.listen(port, () => {
 });
 
 app.get('/index', (req, res) => {
-  res.render('index.ejs', { data: data });
-  console.log(`${req.headers.host}/${req.url}`);
+  db.collection('Users').find().toArray(push); // promise pending
+
+  function push(err, data) {
+    if (err) console.log(err);
+    console.log(data);
+    res.render('index.ejs', { data: data });
+  }
 });
 
 app.get('/mp4', (req, res) => {
@@ -92,13 +66,40 @@ app.get('*', (req, res) => {
   res.render('404.ejs');
 });
 
+// function sendMsg(req, res) {
+//   const msg = {
+//     send: 'true',
+//     message: req.body.message,
+//   };
+
+//   data[0].messages.push(msg);
+
+//   res.redirect('/index');
+// }
+
+// function sendMsg(req, res) {
+//   db.collection('Users').insertOne({
+//     send: true,
+//     message: req.body.message,
+//   });
+
+//   console.log(`A new message has been send: ${req.body.message}`);
+// }
+
 function sendMsg(req, res) {
-  const msg = {
-    send: 'true',
+  let data = {
+    send: true,
     message: req.body.message,
   };
 
-  data[0].messages.push(msg);
+  db.collection('Users').updateOne(
+    {
+      _id: ObjectId('5ecc0dbfc32afdba32b9a75e'),
+    },
+    { $push: { messages: data } }
+  );
 
+  // console.log(data);
+  console.log(`A new message has been send: ${req.body.message}`);
   res.redirect('/index');
 }
