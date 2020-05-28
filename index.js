abfab89ca451196fb0ea0649af2a0b3c2fb24e06
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-28 11:19:56 +0200

    arrow functions and cleanup

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require('express');
 const mongo = require('mongodb');
 const ObjectId = mongo.ObjectID;
+
 // const slug = require('slug')
 const bodyParser = require('body-Parser');
 require('dotenv').config();
 
+const express = require('express');
 const app = express();
 const port = 3000;
+
+let db;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
 
 // Data structure
 // const data = [
@@ -23,13 +27,10 @@ const port = 3000;
 //   },
 // ];
 
-let db;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
-
-mongo.MongoClient.connect(uri, function (err, client) {
+mongo.MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
   db = client.db('DatingApp');
-  db.collection('Users', function (err, collection) {
-    collection.find().toArray(function (err, items) {
+  db.collection('Users', (err, collection) => {
+    collection.find().toArray((err, items) => {
       if (err) throw err;
       // console.log(items);
     });
@@ -47,18 +48,18 @@ app.listen(port, () => {
 });
 
 app.get('/index', (req, res) => {
-  db.collection('Users').find().toArray(push); // promise pending
-
-  function push(err, data) {
-    if (err) console.log(err);
-    console.log(data);
-    res.render('index.ejs', { data: data });
-  }
+  db.collection('Users')
+    .find()
+    .toArray((err, data) => {
+      if (err) console.log(err);
+      // console.log(data);
+      res.render('index.ejs', { data: data });
+    });
 });
 
 app.get('/mp4', (req, res) => {
   res.sendFile(__dirname + '/seal.mp4');
-  console.log(req.headers.host + req.url);
+  // console.log(req.headers.host + req.url);
 });
 
 app.get('*', (req, res) => {
