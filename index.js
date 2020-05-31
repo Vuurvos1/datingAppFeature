9ca4a8eeb0e54bf7a2fc1cc9ff4380ed0051f1f3
Vuurvos1 <Vuurvos1@users.noreply.github.com>
2020-05-31 16:35:50 +0200

    cleanup and socket io

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ let app = express();
 let server = http.createServer(app);
 let io = socketio(server);
 
+let db;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
+
+mongo.MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
+  db = client.db('DatingApp');
+  db.collection('Users', (err, collection) => {
+    collection.find().toArray((err, items) => {
+      if (err) throw err;
+      // console.log(items);
+    });
+  });
+});
+
 // let server = app.listen(process.env.PORT || port, listen);
 
 // let roomname = '';
@@ -21,7 +34,7 @@ app.use(express.static(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.post('/', sendMsg);
+app.post('', sendMsg);
 
 app.get('', (req, res) => {
   db.collection('Users')
@@ -43,34 +56,24 @@ app.get('*', (req, res) => {
 });
 
 /* Database */
-let db;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}`;
-
-mongo.MongoClient.connect(uri, { useUnifiedTopology: true }, (err, client) => {
-  db = client.db('DatingApp');
-  db.collection('Users', (err, collection) => {
-    collection.find().toArray((err, items) => {
-      if (err) throw err;
-      // console.log(items);
-    });
-  });
-});
 
 function sendMsg(req, res) {
-  let data = {
-    send: true,
-    message: req.body.message,
-  };
+  if (req.body.message.trim() != '') {
+    let data = {
+      send: true,
+      message: req.body.message.trim(),
+    };
 
-  db.collection('Users').updateOne(
-    {
-      _id: ObjectId('5ecc0dbfc32afdba32b9a75e'),
-    },
-    { $push: { messages: data } }
-  );
+    db.collection('Users').updateOne(
+      {
+        _id: ObjectId('5ecc0dbfc32afdba32b9a75e'),
+      },
+      { $push: { messages: data } }
+    );
 
-  console.log(`A new message has been send: ${req.body.message}`);
-  res.redirect('/index');
+    console.log(`A new message has been send: ${req.body.message}`);
+    res.redirect('/');
+  }
 }
 
 /* Socket io */
@@ -80,12 +83,30 @@ io.on('connection', (socket) => {
   socket.on('message', (data) => {
     console.log(data.message);
 
-    let data2 = {
-      name: '🦐',
-      message: data.message,
-    };
+    // let data2 = {
+    //   name: '🦐',
+    //   message: data.message,
+    // };
 
-    socket.to(roomName).emit('message', data2);
+    if (data.message.trim() != '') {
+      let DatabaseData = {
+        send: true,
+        message: data.message.trim(),
+      };
+
+      db.collection('Users').updateOne(
+        {
+          _id: ObjectId('5ecc0dbfc32afdba32b9a75e'),
+        },
+        { $push: { messages: DatabaseData } }
+      );
+
+      console.log(`A new message has been send: ${DatabaseData}`);
+      // res.redirect('/');
+    }
+    // add to database
+
+    // socket.to(roomName).emit('message', data2);
   });
 
   socket.on('disconnect', () => {
--- views/js/script.js
+++ views/js/script.js
@@ -1,15 +1,44 @@
 console.log('js connected');
+
+const msg = document.querySelector('#message');
+const sendMsg = document.querySelector('#sendMessage');
+const messageSection = document.querySelector('#messages');
+
 const settings = {
   url: 'http://localhost:3000',
 };
 
 let socket = io(settings.url);
-// let socket = io();
-// socket = ;
 
 let searchRoom = window.location.search.slice(1).trim();
 console.log(searchRoom);
 
 socket.on('message', (data) => {
   console.log(data);
+});
+
+sendMsg.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(msg.value.trim());
+
+  let data = {
+    message: msg.value.trim(),
+  };
+
+  messageSection.innerHTML += `
+    <div class="message messageSend">
+        <p>
+            ${msg.value.trim()}
+        </p>
+    </div>
+    `;
+
+  messageSection.scrollTop = messageSection.scrollHeight;
+
+  socket.emit('message', data);
+  msg.value = '';
+
+  // let data = {
+  //   message: msg.value,
+  // };
 });
